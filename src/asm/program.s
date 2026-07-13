bc74f50842ea09461bd0f93f1808cf01677b62cc
Wirenux <louis.lesniak2@gmail.com>
2026-07-13 17:19:43 +0200

    add: Subroutine test

--- src/asm/program.s
+++ src/asm/program.s
@@ -1,15 +1,12 @@
+; Goal Jump to a routine set X to 5 return and confirm PC
 .org $8000
 
-; --- Test BEQ (Branch if Equal) ---
-LDA #$00    ; Load 0 into A (Sets Zero Flag to 1)
-BEQ forward ; Should branch because Z=1
-LDX #$FF    ; If it doesn't branch, X becomes 0xFF (Failure)
-forward:
-LDX #$01    ; If it branches, X becomes 0x01 (Success)
+START:
+    LDA #$01        ; Load A with 1
+    JSR SUB_ROUTINE ; Jump to address $8005
+    LDA #$02        ; We should end up here after RTS
+    BRK             ; Stop
 
-; --- Test BNE (Branch if Not Equal) ---
-LDA #$05    ; Load 5 into A (Sets Zero Flag to 0)
-BNE branch  ; Should branch because Z=0
-LDY #$FF    ; If it doesn't branch, Y becomes 0xFF (Failure)
-branch:
-LDY #$01    ; If it branches, Y becomes 0x01 (Success)
+SUB_ROUTINE:
+    LDX #$05        ; Set X to 5
+    RTS             ; Return to after JSR
