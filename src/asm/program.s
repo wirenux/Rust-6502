77cb9c5a9302ca224b54c89b285b0a52e66bc485
Wirenux <louis.lesniak2@gmail.com>
2026-07-13 22:55:53 +0200

    add: new item to TODO + new Program to test AND ORA EOR LDA ZeroPageX

--- src/asm/program.s
+++ src/asm/program.s
@@ -1,12 +1,30 @@
-; Goal Jump to a routine set X to 5 return and confirm PC
 .org $8000
 
 START:
-    LDA #$01        ; Load A with 1
-    JSR SUB_ROUTINE ; Jump to address $8005
-    LDA #$02        ; We should end up here after RTS
-    BRK             ; Stop
+    ; --- SETUP Prepare data in the Zero Page ---
+    LDA #$55        ; Load A with $55 (0101 0101 in binary)
+    STA $42         ; Store it at Zero Page address $0042
 
-SUB_ROUTINE:
-    LDX #$05        ; Set X to 5
-    RTS             ; Return to after JSR
+    ; --- 1. Test Indexed Addressing (LDA ZeroPage,X) ---
+    LDX #$02        ; Load X register with 2
+    LDA $40,X       ; Read from Base $40 + X ($02). Reads address $0042.
+                    ; > Accumulator should now be $55 again!
+
+    ; --- 2. Test AND Immediate ---
+    ; A is currently $55 (0101 0101)
+    AND #$0F        ; AND with $0F (0000 1111)
+                    ; > Accumulator should become $05 (0000 0101)
+
+    ; --- 3. Test ORA Immediate ---
+    ; A is currently $05 (0000 0101)
+    ORA #$80        ; OR with $80 (1000 0000)
+                    ; > Accumulator should become $85 (1000 0101)
+                    ; > The Negative flag (N) should now be SET (1)!
+
+    ; --- 4. Test EOR Immediate ---
+    ; A is currently $85 (1000 0101)
+    EOR #$FF        ; XOR with $FF (1111 1111)
+                    ; > Accumulator should become $7A (0111 1010)
+                    ; > The Negative flag (N) should now be CLEARED (0)!
+
+    BRK             ; Stop execution
