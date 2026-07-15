e7145abb1edb0ea4d86d838db5fb71ad3e95726e
Wirenux <louis.lesniak2@gmail.com>
2026-07-15 23:56:04 +0200

    update: program to test STA Absolute X

--- src/asm/program.s
+++ src/asm/program.s
@@ -3,27 +3,44 @@
 .segment "CODE"
 
 reset:
-    clc
-    lda #$42
-    brk
+    ; ------------------------------------------------
+    ; TEST 1: STA Absolute, X (Zero Offset)
+    ; ------------------------------------------------
+    ldx #$00        ; Set X index to 0
+    lda #$42        ; The magic test value
+    sta $0200,X     ; Write to $0200 + 0 = $0200
 
-    cmp #$42
-    bne test_fail
-    bcc test_fail
+    lda $0200       ; Read it back directly
+    cmp #$42        ; Is it $42?
+    bne test_fail   ; If not, branch to failure
 
-    ldx #$FF
-    brk
+    ; ------------------------------------------------
+    ; TEST 2: STA Absolute, X (Positive Offset)
+    ; ------------------------------------------------
+    ldx #$05        ; Set X index to 5
+    lda #$99        ; A different test value
+    sta $0200,X     ; Write to $0200 + 5 = $0205
+
+    lda $0205       ; Read it back directly
+    cmp #$99        ; Is it $99?
+    bne test_fail   ; If not, branch to failure
+
+    ; ------------------------------------------------
+    ; SUCCESS!
+    ; ------------------------------------------------
+    ldx #$FF        ; Load $FF into X to indicate success
+    brk             ; Halt the CPU
 
 interrupt_handler:
     rti
 
 test_fail:
-    ldx #$EE
-    brk
+    ldx #$EE        ; Load $EE into X to indicate failure
+    brk             ; Halt the CPU
 
 
 .segment "VECTORS"
 
-.word 0
-.word reset
-.word interrupt_handler
+.word 0                 ; NMI
+.word reset             ; RESET
+.word interrupt_handler ; IRQ/BRK
