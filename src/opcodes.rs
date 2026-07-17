40612ddf3c8d8ecd45624777ceba48a9434ecc79
Wirenux <louis.lesniak2@gmail.com>
2026-07-17 16:56:38 +0200

    add: Opcode: AND (every addressing Mode)

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,4 +1,6 @@
-use crate::bus::{Bus};
+use std::ptr::addr_of_mut;
+
+use crate::bus::{self, Bus};
 use crate::cpu::{AddressingMode, CPU};
 
 pub fn adc_absolute(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
@@ -34,6 +36,14 @@ pub fn adc_absolute_y(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
     cpu.set_instr(format!("{:02X} {:02X} {:02X}", opcode, low, high), format!("ADC ${:04X},Y", (high as u16) << 8 | low as u16), 4);
 }
 
+pub fn adc_immediate(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
+    let value = bus.read_ram(cpu.pc);
+    cpu.pc += 1;
+    cpu.adc(value);
+
+    cpu.set_instr(format!("{:02X} {:02X}", opcode, value), format!("ADC #${:02X}", value), 2);
+}
+
 pub fn adc_indirect_x(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
     let addr = cpu.get_operand_address(&AddressingMode::IndirectX, bus);
     let value = bus.read_ram(addr);
@@ -54,14 +64,6 @@ pub fn adc_indirect_y(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
     cpu.set_instr(format!("{:02X} {:02X}", opcode, ptr), format!("ADC (${:02X},Y)", ptr), 6);
 }
 
-pub fn adc_immediate(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
-    let value = bus.read_ram(cpu.pc);
-    cpu.pc += 1;
-    cpu.adc(value);
-
-    cpu.set_instr(format!("{:02X} {:02X}", opcode, value), format!("ADC #${:02X}", value), 2);
-}
-
 pub fn adc_zeropage(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
     let addr = cpu.get_operand_address(&AddressingMode::ZeroPage, bus);
     let value = bus.read_ram(addr);
@@ -79,6 +81,46 @@ pub fn adc_zeropage_x(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
     cpu.set_instr(format!("{:02X} {:02X}", opcode, base_addr), format!("ADC ${:02X},X", base_addr), 4);
 }
 
+pub fn and_absolute(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
+    let addr = cpu.get_operand_address(&AddressingMode::Absolute, bus);
+    let value = bus.read_ram(addr);
+
+    cpu.reg_a &= value;
+
+    cpu.update_z_n_flags(cpu.reg_a);
+
+    let low = (addr & 0xFF) as u8;
+    let high = (addr >> 8) as u8;
+    cpu.set_instr(format!("{:02X} {:02X} {:02X}", opcode, low, high), format!("AND ${:04X}", addr), 4);
+}
+
+pub fn and_absolute_x(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
+    let addr = cpu.get_operand_address(&AddressingMode::AbsoluteX, bus);
+    let value = bus.read_ram(addr);
+
+    cpu.reg_a &= value;
+
+    cpu.update_z_n_flags(cpu.reg_a);
+
+    let low = bus.read_ram(cpu.pc - 2);
+    let high = bus.read_ram(cpu.pc - 1);
+    cpu.set_instr(format!("{:02X} {:02X} {:02X}", opcode, low, high), format!("AND ${:04X},X", (high as u16) << 8 | low as u16), 4);
+}
+
+pub fn and_absolute_y(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
+    let addr = cpu.get_operand_address(&AddressingMode::AbsoluteY, bus);
+    let value = bus.read_ram(addr);
+
+    cpu.reg_a &= value;
+
+    cpu.update_z_n_flags(cpu.reg_a);
+
+    let low = bus.read_ram(cpu.pc - 2);
+    let high = bus.read_ram(cpu.pc - 1);
+    cpu.set_instr(format!("{:02X} {:02X} {:02X}", opcode, low, high), format!("AND ${:04X},Y", (high as u16) << 8 | low as u16), 4);
+}
+
+
 pub fn and_immediate(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
     let addr = cpu.get_operand_address(&AddressingMode::Immediate, bus);
     let value = bus.read_ram(addr);
@@ -89,6 +131,53 @@ pub fn and_immediate(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
 
     cpu.set_instr(format!("{:02X} {:02X}", opcode, value), format!("AND #${:02X}", value), 2);
 }
+
+pub fn and_indirect_x(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
+    let addr = cpu.get_operand_address(&AddressingMode::IndirectX, bus);
+    let value = bus.read_ram(addr);
+
+    cpu.reg_a &= value;
+
+    cpu.update_z_n_flags(cpu.reg_a);
+
+    let ptr = bus.read_ram(cpu.pc - 1);
+    cpu.set_instr(format!("{:02X} {:02X}", opcode, ptr), format!("AND (${:02X},X)", ptr), 6);
+}
+
+pub fn and_indirect_y(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
+    let addr = cpu.get_operand_address(&AddressingMode::IndirectY, bus);
+    let value = bus.read_ram(addr);
+
+    cpu.reg_a &= value;
+
+    cpu.update_z_n_flags(cpu.reg_a);
+
+    let ptr = bus.read_ram(cpu.pc - 1);
+    cpu.set_instr(format!("{:02X} {:02X}", opcode, ptr), format!("AND (${:02X},Y)", ptr), 6);
+}
+
+pub fn and_zeropage(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
+    let addr = cpu.get_operand_address(&AddressingMode::ZeroPage, bus);
+    let value = bus.read_ram(addr);
+
+    cpu.reg_a &= value;
+    cpu.update_z_n_flags(cpu.reg_a);
+
+    let op_byte = addr as u8;
+    cpu.set_instr(format!("{:02X} {:02X}", opcode, op_byte), format!("AND ${:02X}", op_byte), 3);
+}
+
+pub fn and_zeropage_x(cpu: &mut CPU, bus: &mut Bus, opcode: u8) {
+    let addr = cpu.get_operand_address(&AddressingMode::ZeroPageX, bus);
+    let value = bus.read_ram(addr);
+
+    cpu.reg_a &= value;
+    cpu.update_z_n_flags(cpu.reg_a);
+
+    let base_addr = bus.read_ram(cpu.pc - 1);
+    cpu.set_instr(format!("{:02X} {:02X}", opcode, base_addr), format!("AND ${:02X},X", base_addr), 4);
+}
+
 
 pub fn asl_accumulator(cpu: &mut CPU, opcode: u8) {
     let left_byte = (cpu.reg_a & 0x80) >> 7; // save the edge byte
