87b1184cc17e26fa664177407564a3e02bcb424e
Wirenux <louis.lesniak2@gmail.com>
2026-07-20 18:58:41 +0200

    add: test screen program

--- src/asm/program.s
+++ src/asm/program.s
@@ -1,14 +1,23 @@
+TMP = $00
+
 .segment "CODE"
 
 start:
     ldx #$00
-    lda #$02
 
 fill_loop:
+    txa
+    lsr
+    lsr
+    lsr
+    lsr
+    lsr
+    sta TMP
+
+    txa
+    clc
+    adc TMP
     sta $0200,X ; page 0
-    sta $0300,X ; page 1
-    sta $0400,X ; page 2
-    sta $0500,X ; page 3
 
     inx
     bne fill_loop
