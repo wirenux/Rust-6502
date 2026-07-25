79d33f7ddb8ae10855c78285701cfe6898adde17
Wirenux <louis.lesniak2@gmail.com>
2026-07-25 23:17:58 +0200

    add: comment to program.s

--- src/asm/program.s
+++ src/asm/program.s
@@ -1,11 +1,11 @@
 .segment "CODE"
 
 start:
-  lda #$01
+  lda #$01 ; Load 1 into the A register
 
 loop:
-  ror
-  sta $0200
+  ror ; Rotate to the right the value present in the A register
+  sta $0200 ; Store the value present in the A register at $0200 (im memory)
   jmp loop
 
 .segment "VECTORS"
